b46c980c0ad3873f77f57fd5c1d51ffe67b11a89
Cryppen <34452759+Cryppen@users.noreply.github.com>
2019-10-18 13:47:57 +0200

    created advanced models in model folder

---
+++ Models/Model_Version_1_01.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, ZeroPadding2D, Input
+from tensorflow.keras import Model
+
+
+class Model_Version_1_01(tf.keras.Model):
+    def __init__(self):
+        super(Model_Version_1_01, self).__init__()
+
+        # Adding 2 layers of padding on each side, temporarily making the image 303x303
+        self.padding = ZeroPadding2D(padding=(2, 2),  # Amount of Zeros on each side of the input
+                                     data_format='channels_last',  # format of batch, height, width, channels
+                                     )
+        # first convolutional layer
+        self.conv1 = Conv2D(16,  # filters
+                            (3, 3),  # Kernel size
+                            strides=(1, 1),  # Stride
+                            padding='valid',  # Same refers to same padding as previous layer.
+                            data_format=None,
+                            # It should be defined if the dimensions are structured in non standard approach
+                            dilation_rate=(1, 1),  # how dilated the picture is
+                            activation='relu',  # Activation function
+                            use_bias=True,  # Enable bias
+                            kernel_initializer='glorot_uniform',  # initialiser of filters
+                            bias_initializer='zeros',  # initialisation of bias
+                            kernel_regularizer=None,  #
+                            bias_regularizer=None,  #
+                            activity_regularizer=None,  #
+                            kernel_constraint=None,  #
+                            bias_constraint=None,  #
+                            )
+
+        self.maxpool1 = MaxPooling2D(pool_size=(2, 2),  # pool size
+                                     strides=(2, 2),  # stride size
+                                     padding='same',  # padding
+                                     data_format=None)  #
+
+        # Second convolutional layer
+        self.conv2 = Conv2D(32,  # filters
+                            (3, 3),  # Kernel size
+                            strides=(1, 1),  # Stride
+                            padding='same',  # Same refers to same padding as previous layer.
+                            data_format=None,
+                            # It should be defined if the dimensions are structured in non standard approach
+                            dilation_rate=(1, 1),  # how dilated the picture is
+                            activation='relu',  # Activation function
+                            use_bias=True,  # Enable bias
+                            kernel_initializer='glorot_uniform',  # initialiser of filters
+                            bias_initializer='zeros',  # initialisation of bias
+                            kernel_regularizer=None,  #
+                            bias_regularizer=None,  #
+                            activity_regularizer=None,  #
+                            kernel_constraint=None,  #
+                            bias_constraint=None,  #
+                            )
+
+        # 3rd convolutional layer
+        self.conv3 = Conv2D(48,  # filters
+                            (3, 3),  # Kernel size
+                            strides=(1, 1),  # Stride
+                            padding='same',  # Same refers to same padding as previous layer.
+                            data_format=None,
+                            # It should be defined if the dimensions are structured in non standard approach
+                            dilation_rate=(1, 1),  # how dilated the picture is
+                            activation='relu',  # Activation function
+                            use_bias=True,  # Enable bias
+                            kernel_initializer='glorot_uniform',  # initialiser of filters
+                            bias_initializer='zeros',  # initialisation of bias
+                            kernel_regularizer=None,  #
+                            bias_regularizer=None,  #
+                            activity_regularizer=None,  #
+                            kernel_constraint=None,  #
+                            bias_constraint=None,  #
+                            )
+
+        self.maxpool2 = MaxPooling2D(pool_size=(2, 2),  # pool size
+                                     strides=(2, 2),  # stride size
+                                     padding='same',  # padding
+                                     data_format=None)  #
+
+        self.flatten = Flatten()
+
+        # Dense is a fully connected layer
+        self.d1 = Dense(128,  # Amount of neurons
+                        activation='relu',  # Activation function
+                        use_bias=True,  # bias is enabled
+                        bias_initializer='zeros',  # initialisation of bias
+                        bias_regularizer=None,  # regularize biases
+                        activity_regularizer=None,  #
+                        bias_constraint=None)  #
+
+        # Dense is a fully connected layer
+        self.d2 = Dense(128,  # Amount of neurons
+                        activation='relu',  # Activation function
+                        use_bias=True,  # bias is enabled
+                        bias_initializer='zeros',  # initialisation of bias
+                        bias_regularizer=None,  # regularize biases
+                        activity_regularizer=None,  #
+                        bias_constraint=None)  #
+
+        self.d3 = Dense(5,  # Amount of neurons
+                        activation='softmax',  # Activation function
+                        use_bias=True,  # bias is enabled
+                        bias_initializer='zeros',  # initialisation of bias
+                        bias_regularizer=None,  # regularize biases
+                        activity_regularizer=None,  #
+                        bias_constraint=None)  #
+
+    # Call method should include all layers from model.
+    def call(self, x):
+        x = self.padding(x)
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        return self.d3(x)
+
+    def model(self):
+        x = Input(shape=(299, 299, 1))
+        return Model(inputs=[x], outputs=self.call(x))
--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from tensorflow.keras import Model
-import os
-from callback import *
 from data_preprocess import *
 from post_processing import *
-from sequential_model import *
-from advanced_model import *
-from fastcompile import *
+from Models.sequential_model import *
+from Models.Model_Version_1_01 import *
 
 # When changes are made to the constants, changes need to be done in sequential_model as well
 FILE_SIZE = 7305  # Training dataset size
@@ -20,17 +12,16 @@ BATCH_SIZE = 32
 parsed_training_data, parsed_val_data, parsed_testing_data = process_dataset()
 
 # batching the dataset into 32-size minibatches
-batched_training_data = parsed_training_data.shuffle(buffer_size=1024).batch(BATCH_SIZE)    # BATCH_SIZE
-batched_val_data = parsed_val_data.batch(BATCH_SIZE).repeat()                               # BATCH_SIZE
-batched_testing_data = parsed_testing_data.batch(BATCH_SIZE).repeat()                       # BATCH_SIZE
-
+batched_training_data = parsed_training_data.batch(BATCH_SIZE)                   # BATCH_SIZE
+batched_val_data = parsed_val_data.batch(BATCH_SIZE)                             # BATCH_SIZE
+batched_testing_data = parsed_testing_data.batch(BATCH_SIZE)                       # BATCH_SIZE
 
 # initializing the callback
 callback = myCallback()
 tb_callback = tensorboard_callback("logs", 1)
 cp_callback = checkpoint_callback()
 
-model = MyModel()
+model = Model_Version_1_01()
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
@@ -41,15 +32,20 @@ history = model.fit(
         validation_data = batched_val_data,
         validation_steps = TEST_SIZE // BATCH_SIZE,  # TEST_SIZE
         epochs=15,
+        shuffle=True,
         verbose=1,  # verbose is the progress bar when training
         callbacks=[callback, cp_callback, tb_callback]
 )
 
-# # Evaluate the model on unseen testing data
+# Evaluate the model on unseen testing data
 print('\n# Evaluate on test data')
 results = model.evaluate(batched_testing_data, steps= TEST_SIZE // BATCH_SIZE)
 print('test loss, test acc:', results)
 
+if __name__ == '__main__':
+    sub = Model_Version_1_01()
+    sub.model().summary()
+
 # History displaying training and validation accuracy
-plot_history(history)
 plot_multi_label_predictions(batched_testing_data, model, 10)
+plot_history(history)
