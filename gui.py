854871369637a6f708370eceff3cc03102105329
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2019-10-23 02:29:48 +0200

    Fuckeh

--- gui.py
+++ gui.py
@@ -1,19 +1,19 @@
 import sys
 
+import PyQt5
 import numpy as np
 from PIL import Image
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtCore
 from PyQt5.Qt import Qt
 from PyQt5.QtChart import QChart, QChartView, QBarCategoryAxis, QBarSet, QBarSeries, QValueAxis
-from PyQt5.QtCore import pyqtSlot, QDir
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QVBoxLayout, QPushButton, QHBoxLayout, \
-    QFileSystemModel, QTreeView
+from PyQt5.QtCore import QDir
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QVBoxLayout, QHBoxLayout, \
+    QFileSystemModel, QTreeView, QTextEdit, QListView
 
 from models.Model_Version_1_01 import *
 
-
 # Makes the application scale correct on all resolutions
-# PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 
 
 class App(QWidget):
@@ -22,8 +22,8 @@ class App(QWidget):
         self.title = 'Mammogram Prediction'
         self.left = 100
         self.top = 100
-        self.width = 900
-        self.height = 500
+        self.width = 1000
+        self.height = 600
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
@@ -34,16 +34,21 @@ class App(QWidget):
         prediction = self.makePrediction(self.getModel(), self.convertPictureToNumpy(input_image))
 
         # Widget for showing picture. The QLabel gets a Pixmap added to it
+        self.picture_name_label = QLabel(input_image)
         self.picture_label = QLabel(self)
-        picture = QtGui.QPixmap(input_image)
-        self.picture_label.setPixmap(picture)
+        self.picture = QtGui.QPixmap(input_image)
+        self.picture_label.setPixmap(self.picture)
 
-        # Normal text labels to show predictions
-        self.label_negative = QLabel("Probability of Negative: %s" % prediction[0, 0], self)
-        self.label_benign_cal = QLabel("Probability of benign calcification: %s" % prediction[0, 1], self)
-        self.label_benign_mass = QLabel("Probability of benign mass: %s" % prediction[0, 2], self)
-        self.label_malignant_cal = QLabel("Probability of malignant calcification: %s" % prediction[0, 3], self)
-        self.label_malignant_mass = QLabel("Probability of malignant mass: %s" % prediction[0, 4], self)
+        # Widget for adding prediction text
+        self.prediction_text = QTextEdit()
+        self.prediction_text.append("Probability of Negative: %s" % prediction[0, 0] +
+                                    "\n\nProbability of benign calcification: %s" % prediction[0, 1] +
+                                    "\n\nProbability of benign mass: %s" % prediction[0, 2] +
+                                    "\n\nProbability of malignant calcification: %s" % prediction[0, 3] +
+                                    "\n\nProbability of malignant mass: %s" % prediction[0, 4])
+        self.prediction_text.setReadOnly(True)
+        # self.prediction_text.setMaximumSize(400, 299)
+        # self.prediction_text.setMinimumSize(400, 299)
 
         # QChartView to show a visual representation of probabilities
         self.diag_set_neg = QBarSet('Negative')
@@ -68,73 +73,93 @@ class App(QWidget):
         self.chart.addSeries(self.series)
         self.chart.setTitle('Diagnosis Prediction Chart')
 
-        axisX = QBarCategoryAxis()
-        axisY = QValueAxis()
-        axisY.setRange(0, 1)
-        self.chart.addAxis(axisX, Qt.AlignBottom)
-        self.chart.addAxis(axisY, Qt.AlignLeft)
+        self.axisX = QBarCategoryAxis()
+        self.axisY = QValueAxis()
+        self.axisY.setRange(0, 1)
+        self.chart.addAxis(self.axisX, Qt.AlignBottom)
+        self.chart.addAxis(self.axisY, Qt.AlignLeft)
         self.chart.legend().setVisible(True)
         self.chart.legend().setAlignment(Qt.AlignBottom)
         self.chartView = QChartView(self.chart)
-        self.chartView.setMaximumSize(600, 320)
+        self.chartView.setMaximumSize(400, 320)
+        self.chartView.setMinimumSize(400, 320)
 
-        self.directory = QFileSystemModel()
-        self.index = self.directory.index(QDir.currentPath())
-        self.tree = QTreeView(parent=self)
-        self.tree.setModel(self.directory)
-        self.tree.expand(self.index)
-        self.tree.setAnimated(False)
-        self.tree.setIndentation(20)
-        self.tree.setSortingEnabled(True)
+        # Tree and List view for file directory overview
+        path = '/pictures'
+        self.treeview = QTreeView()
+        self.listview = QListView()
+
+        self.dirModel = QFileSystemModel()
+        self.dirModel.setRootPath(path)
+        self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
+
+        self.fileModel = QFileSystemModel()
+        self.fileModel.setFilter(QDir.NoDotAndDotDot | QDir.Files)
+
+        self.treeview.setModel(self.dirModel)
+        self.listview.setModel(self.fileModel)
+
+        self.treeview.setRootIndex(self.dirModel.index(path))
+        self.listview.setRootIndex(self.fileModel.index(path))
+        self.treeview.clicked.connect(self.on_clicked)
+
+        # self.directory = QFileSystemModel()
+        # self.index = self.directory.index(QDir.currentPath())
+        # self.tree = QTreeView(parent=self)
+        # self.tree.setModel(self.directory)
+        # self.tree.expand(self.index)
+        # self.tree.setAnimated(False)
+        # self.tree.setIndentation(20)
+        # self.tree.setSortingEnabled(True)
+        # self.tree.setMaximumSize(500, 300)
+        # self.tree.setMinimumSize(500, 300)
 
         # Button to open new picture file
-        self.button = QPushButton('Open New Image', self)
-        self.button.setMaximumSize(140, 30)
-        self.button.clicked.connect(self.on_click)
+        # self.button = QPushButton('Open New Image', self)
+        # self.button.setMaximumSize(140, 30)
+        # self.button.clicked.connect(self.on_clicked)
 
-        # Layout handling. All widgets gets added to the vbox.
-        hbox_top = QHBoxLayout()
-        hbox_buttom = QHBoxLayout()
+        # Layout handling.
+        self.vbox = QVBoxLayout()
+        self.hbox_top = QHBoxLayout()
+        self.hbox_buttom = QHBoxLayout()
+        self.setLayout(self.vbox)  # This vbox is the outer layer
 
-        hbox_top.addWidget(self.tree)
-        hbox_top.addWidget(self.picture_label)
-        hbox_buttom.addWidget(self.chartView, alignment=Qt.AlignLeft | Qt.AlignCenter)
+        self.vbox.addLayout(self.hbox_top)
+        self.vbox.addLayout(self.hbox_buttom)
 
-        vbox = QVBoxLayout()
-        vbox_inside = QVBoxLayout()
+        # Adding widgets to layouts
+        self.vbox.addWidget(self.picture_name_label)
+        self.hbox_top.addWidget(self.treeview)
+        self.hbox_top.addWidget(self.listview)
+        self.hbox_buttom.addWidget(self.picture_label, alignment=Qt.AlignLeft)
+        self.hbox_buttom.addWidget(self.prediction_text, alignment=Qt.AlignLeft)
+        # self.hbox_buttom.addWidget(self.chartView, alignment=Qt.AlignLeft | Qt.AlignCenter)
+        # self.vbox.addWidget(self.button)
 
-        vbox.addLayout(hbox_top)
-        vbox.addLayout(hbox_buttom)
-
-        vbox_inside.addWidget(self.label_negative)
-        vbox_inside.addWidget(self.label_benign_cal)
-        vbox_inside.addWidget(self.label_benign_mass)
-        vbox_inside.addWidget(self.label_malignant_cal)
-        vbox_inside.addWidget(self.label_malignant_mass)
-        hbox_buttom.addLayout(vbox_inside)
-        vbox.addWidget(self.button)
-        self.setLayout(vbox)
-
-        p = self.palette()
-        p.setColor(self.backgroundRole(), Qt.white)
-        self.setPalette(p)
-        self.setFixedSize(self.size())
+        # p = self.palette()
+        # p.setColor(self.backgroundRole(), Qt.white)
+        # self.setPalette(p)
         self.show()
 
-    @pyqtSlot()
-    def on_click(self):
-        # new_picture, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "Vælg Billede",
-        #         #                                              "All Files (*);;Python Files (*.py)")
-        new_picture = self.tree.get
+    def on_clicked(self, index):
+        path = self.dirModel.fileInfo(index).absoluteFilePath()
+        self.listview.setRootIndex(self.fileModel.setRootPath(path))
 
-        model = self.getModel()
-        new_prediction = self.makePrediction(model, self.convertPictureToNumpy(new_picture))
-        self.picture_label.setPixmap(QtGui.QPixmap(new_picture))
-        self.label_negative.setText("Probability of negative: %s" % new_prediction[0, 0])
-        self.label_benign_cal.setText("Probability of benign calcification: %s" % new_prediction[0, 1])
-        self.label_benign_mass.setText("Probability of benign mass: %s" % new_prediction[0, 2])
-        self.label_malignant_cal.setText("Probability of malignant calcification: %s" % new_prediction[0, 3])
-        self.label_malignant_mass.setText("Probability of malignant mass: %s" % new_prediction[0, 4])
+    # @pyqtSlot()
+    # def on_click(self):
+    #     model = self.getModel()
+    #     new_picture, _ = self.listview.getOpenFileName(self, "QFileDialog.getOpenFileName()", "Vælg Billede",
+    #                                                  "All Files (*);;Python Files (*.py)")
+    #     new_prediction = self.makePrediction(model, self.convertPictureToNumpy(new_picture))
+    #
+    #     self.picture_name_label.setText(new_picture)
+    #     self.picture_label.setPixmap(QtGui.QPixmap(new_picture))
+    #     self.prediction_text.setText("Probability of Negative: %s" % new_prediction[0, 0] +
+    #                                  "\n\nProbability of benign calcification: %s" % new_prediction[0, 1] +
+    #                                  "\n\nProbability of benign mass: %s" % new_prediction[0, 2] +
+    #                                  "\n\nProbability of malignant calcification: %s" % new_prediction[0, 3] +
+    #                                  "\n\nProbability of malignant mass: %s" % new_prediction[0, 4])
 
     def getModel(self):
         model = Model_Version_1_01()
