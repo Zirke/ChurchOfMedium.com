6c624c15ff412ccb816f7a2d2ab5f31a36678dc7
abiram98 <abiram98@gmail.com>
2019-11-01 09:04:48 +0100

    epoch main commit
     - 2 standard models for training
      - Model_Version_1_06c.py
      - Model_Version_2_05c.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from models.Model_Version_2_05c import Model_Version_2_05c
 from sorting_hub import *
 from callback import *
 from data_Processing.post_processing import *
@@ -7,7 +8,6 @@ from models.Model_Version_2_01c import *
 import os
 import shutil
 import webbrowser
-
 """
 Get datasets for training, validation, and testing
 process_data(file_path) gives a binary classification dataset, list of all file paths in sorting_hub
@@ -15,11 +15,12 @@ process_dataset() gives dataset for 5 classes dataset
 
 from data_Processing.binary_pre_processing import *
 """
-parsed_training_data, parsed_val_data, parsed_testing_data = process_data(five_diagnosis_paths)
+parsed_training_data, parsed_val_data, parsed_testing_data = process_data(negative_bi_file_paths)
 
 FILE_SIZE = len(list(parsed_training_data))  # Training dataset size
 TEST_SIZE = len(list(parsed_val_data))  # Validation and test dataset size
 BATCH_SIZE = 32
+EPOCHS = 10
 
 parsed_training_data = parsed_training_data.shuffle(buffer_size=FILE_SIZE,
                                                     seed=None,
@@ -30,13 +31,15 @@ parsed_val_data = parsed_val_data.shuffle(buffer_size=TEST_SIZE,
                                           reshuffle_each_iteration=True)
 
 # batching the dataset into 32-size mini-batches
-batched_training_data = parsed_training_data.batch(BATCH_SIZE).repeat()  # BATCH_SIZE
-batched_val_data = parsed_val_data.batch(BATCH_SIZE).repeat()  # BATCH_SIZE
-batched_testing_data = parsed_testing_data.batch(BATCH_SIZE).repeat()  # BATCH_SIZE
+batched_training_data = parsed_training_data.batch(BATCH_SIZE).repeat(EPOCHS)  # BATCH_SIZE
+batched_val_data = parsed_val_data.batch(BATCH_SIZE).repeat(EPOCHS)  # BATCH_SIZE
+batched_testing_data = parsed_testing_data.batch(BATCH_SIZE)  # BATCH_SIZE
 
 # Clear Tensorboard
 if os.path.isdir('logs'):
     shutil.rmtree('logs')
+
+model = Model_Version_2_05c()
 
 # initializing the callback
 es_callback = early_stopping_callback('val_loss', 5)
@@ -45,10 +48,8 @@ tb_callback = tensorboard_callback("logs", 1)
 model_string = str(model).split(".")
 cp_callback = checkpoint_callback(str(model_string[len(model_string) - 2]))
 
-model = Model_Version_2_01c()
-
 if __name__ == '__main__':
-    sub = Model_Version_2_01c()
+    sub = Model_Version_2_05c()
     sub.model().summary()
 
 model.compile(optimizer='adam',
@@ -60,7 +61,7 @@ history = model.fit(
     steps_per_epoch=FILE_SIZE // BATCH_SIZE,  # FILE_SIZE
     validation_data=batched_testing_data,
     validation_steps=TEST_SIZE // BATCH_SIZE,  # TEST_SIZE
-    epochs=10,
+    epochs=EPOCHS,
     shuffle=True,
     verbose=2,  # ,  # verbose is the progress bar when training
     callbacks=[es_callback, cp_callback, tb_callback]
@@ -72,7 +73,7 @@ results = model.evaluate(batched_val_data, steps=TEST_SIZE // BATCH_SIZE)
 print('test loss, test acc:', results)
 
 # History displaying training and validation accuracy
-plot_multi_label_predictions(batched_testing_data, model, 10)
+plot_binary_label_predictions(batched_testing_data, model, 10)
 plot_history(history)
 
 # Open Tensorboard
