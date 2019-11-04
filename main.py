02f3d280889a35f8ac5f905ad9e017bbde466004
Zirke <defrenefre@hotmail.com>
2019-11-04 13:49:02 +0100

    test

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from models.Model_Version_2_05c import Model_Version_2_05c
 from sorting_hub import *
 from callback import *
 from data_Processing.post_processing import *
 from data_Processing.pre_processing import *
 from data_Processing.binary_pre_processing import *
 from models.Model_Version_2_01c import *
+from models import *
 import os
 import shutil
 import webbrowser
@@ -15,12 +15,12 @@ process_dataset() gives dataset for 5 classes dataset
  
 from data_Processing.binary_pre_processing import *
 """
-parsed_training_data, parsed_val_data, parsed_testing_data = process_data(malignant_cal_split_paths)
+parsed_training_data, parsed_val_data, parsed_testing_data = process_data(five_diagnosis_paths)
  
 FILE_SIZE = len(list(parsed_training_data))  # Training dataset size
 TEST_SIZE = len(list(parsed_val_data))  # Validation and test dataset size
 BATCH_SIZE = 32
-EPOCHS = 50
+EPOCHS = 5
  
 parsed_training_data = parsed_training_data.shuffle(buffer_size=FILE_SIZE,
                                                     seed=None,
@@ -39,31 +39,31 @@ batched_testing_data = parsed_testing_data.batch(BATCH_SIZE)  # BATCH_SIZE
 if os.path.isdir('logs'):
     shutil.rmtree('logs')
  
-model = Model_Version_2_05c()
+model = Model_Version_1_06c()
  
 # initializing the callback
 es_callback = early_stopping_callback('val_loss', 5)
 ms_callback = manual_stopping_callback()
 tb_callback = tensorboard_callback("logs", 1)
 model_string = str(model).split(".")
-cp_callback = checkpoint_callback(str(model_string[len(model_string) - 2]), 'binary', 'mc')
+cp_callback = checkpoint_callback(str(model_string[len(model_string) - 2]))
  
 if __name__ == '__main__':
-    sub = Model_Version_2_05c()
+    sub = model
     sub.model().summary()
  
 model.compile(optimizer='sgd',
               loss='categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy', tf.keras.metrics.Recall(), tf.keras.metrics.Precision()])
  
 history = model.fit(
     batched_training_data,
-    steps_per_epoch=100 // BATCH_SIZE,  # FILE_SIZE
+    steps_per_epoch=FILE_SIZE // BATCH_SIZE,  # FILE_SIZE
     validation_data=batched_testing_data,
     validation_steps=TEST_SIZE // BATCH_SIZE,  # TEST_SIZE
     epochs=EPOCHS,
     shuffle=True,
-    verbose=2,  # ,  # verbose is the progress bar when training
+    verbose=1,  # ,  # verbose is the progress bar when training
     callbacks=[es_callback, cp_callback, tb_callback]
 )
  
