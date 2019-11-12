2ccca8b3bf1bd239d04a8c7bf4397e16eeed90a7
Zirke <defrenefre@hotmail.com>
2019-11-12 11:50:00 +0100

    k

--- main.py
+++ main.py
@@ -11,13 +11,26 @@ import webbrowser
 import keras_metrics
 
 """
-Get datasets for training, validation, and testing
-process_data(file_path) gives a binary classification dataset, list of all file paths in sorting_hub
-process_dataset() gives dataset for 5 classes dataset
+How to use main.py :
 
-from data_Processing.binary_pre_processing import *
+- Enter model;
+Five Category is model = Model_Version_1_06c()
+Binary Model is model = Model_Version_2_05c()
+
+To change dataset set CONTROL_VARIABLE to one of the following:
+ - 'Five'
+ - 'Negative'
+ - 'BenignC'
+ - 'BenignM'
+ - 'MalignantC'
+ - 'MalignantM'
 """
-parsed_training_data, parsed_val_data, parsed_testing_data = process_data(malignant_mass_split_paths)
+# TODO add model to CONTROL_VARIABLE
+MODEL = Model_Version_2_05f()
+CONTROL_VARIABLE = 'MalignantM'
+
+path_holder, type_holder, class_holder = main_management(CONTROL_VARIABLE)
+parsed_training_data, parsed_val_data, parsed_testing_data = process_data(path_holder)
 
 FILE_SIZE = len(list(parsed_training_data))  # Training dataset size
 TEST_SIZE = len(list(parsed_val_data))  # Validation and test dataset size
@@ -28,59 +41,57 @@ EPOCHS = 50
 batched_training_data = parsed_training_data.batch(BATCH_SIZE).repeat(EPOCHS)  # BATCH_SIZE
 batched_val_data = parsed_val_data.batch(BATCH_SIZE).repeat(EPOCHS)  # BATCH_SIZE
 batched_testing_data = parsed_testing_data.batch(BATCH_SIZE)  # BATCH_SIZE
-print("here")
+
 # Clear Tensorboard
 if os.path.isdir('logs'):
     shutil.rmtree('logs')
 
-model = Model_Version_2_05c()
-
 # initializing the callback
-es_callback = early_stopping_callback('val_categorical_accuracy', 5)
+es_callback = early_stopping_callback('val_loss', 5)
 ms_callback = manual_stopping_callback()
 tb_callback = tensorboard_callback("logs", 1)
-model_string = str(model).split(".")
-cp_callback = checkpoint_callback(str(model_string[len(model_string) - 2]), 'binary', 'mc')
+model_string = str(MODEL).split(".")
+cp_callback = checkpoint_callback(str(model_string[len(model_string) - 2]), type_holder, class_holder)
 
 if __name__ == '__main__':
-    sub = model
+    sub = MODEL
     sub.model().summary()
 
-model.compile(optimizer=tf.keras.optimizers.Adam(),
+MODEL.compile(optimizer=tf.keras.optimizers.Adam(),
               loss=tf.keras.losses.CategoricalCrossentropy(),
-              metrics=[tf.metrics.CategoricalAccuracy(), keras_metrics.categorical_false_negative(),
-                       keras_metrics.categorical_false_positive(), keras_metrics.recall()])
+              metrics=[tf.metrics.CategoricalAccuracy(),
+                       keras_metrics.categorical_false_negative(),
+                       keras_metrics.categorical_false_positive(),
+                       keras_metrics.categorical_metric(),
+                       keras_metrics.categorical_true_negative(),
+                       keras_metrics.categorical_true_positive(),
+                       keras_metrics.precision(),
+                       keras_metrics.recall()])
 
-history = model.fit(
+history = MODEL.fit(
     batched_training_data,
     steps_per_epoch=FILE_SIZE // BATCH_SIZE,  # FILE_SIZE
     validation_data=batched_testing_data,
     validation_steps=TEST_SIZE // BATCH_SIZE,  # TEST_SIZE
     epochs=EPOCHS,
     shuffle=True,
-    verbose=2,  # ,  # verbose is the progress bar when training
-    callbacks=[cp_callback, es_callback]
+    verbose=2,
+    callbacks=[cp_callback]
 )
 
 # Evaluate the model on unseen testing data
 print('\n# Evaluate on test data')
-results = model.evaluate(batched_val_data, steps=TEST_SIZE // BATCH_SIZE)
+results = MODEL.evaluate(batched_val_data, steps=TEST_SIZE // BATCH_SIZE)
 print('test loss, test acc:', results)
 
 # History displaying training and validation accuracy
-plot_binary_label_predictions(batched_testing_data, model, 10)
+if type_holder == 'five':
+    plot_multi_label_predictions(batched_testing_data, MODEL, 10)
+elif type_holder == 'binary':
+    plot_binary_label_predictions(batched_testing_data, MODEL, 10)
+
 plot_history(history)
 
 # Open Tensorboard
 webbrowser.open('http://localhost:6006/')
 os.system('tensorboard --logdir logs/')
-
-
-def shuffle(parsed_training_data, parsed_val_data):
-    parsed_training_data = parsed_training_data.shuffle(buffer_size=FILE_SIZE,
-                                                        seed=None,
-                                                        reshuffle_each_iteration=True)
-
-    parsed_val_data = parsed_val_data.shuffle(buffer_size=TEST_SIZE,
-                                              seed=None,
-                                              reshuffle_each_iteration=True)
