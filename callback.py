4f5cb88d3ff5554783a074af0f4afab6759dc126
abiram98 <abiram98@gmail.com>
2019-11-19 15:18:22 +0100

    confusion matrix

--- callback.py
+++ callback.py
@@ -1,8 +1,9 @@
 import datetime
 import os
 import sys
-
+import re
 import tensorflow as tf
+import numpy as np
 
 
 # Callback that stops training when it reaches a single
@@ -11,12 +12,6 @@ class manual_stopping_callback(tf.keras.callbacks.Callback):
         if (logs.get('accuracy') - logs.get('val_accuracy')) > 0.1:
             print("\nTraining Accuracy is too large compared to validation accuracy")
             self.model.stop_training = True
-
-
-"""
-class tensorboard_callback(tf.keras.callbacks.TensorBoard):
-    def on_train_batch_end(self, batch, logs=None):
-"""
 
 
 def early_stopping_callback(metrics, patience):
@@ -64,13 +59,101 @@ class SavePredCallback(tf.keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         file = open(self.file_path, "a")
-        file.write('Epoch :' + str(epoch)+'\n')
+        file.write('Epoch :' + str(epoch) + '\n')
         batch_counter = 0
         self.out_log.append(epoch)
         for batch, label_batch in self.val_data:
             file.write("batch :" + str(batch_counter) + '\n')
             for image, label in zip(batch, label_batch):
-                image = tf.reshape(image.numpy(), [1,299, 299,1])
+                image = tf.reshape(image.numpy(), [1, 299, 299, 1])
                 file.write(str(self.model.predict(image)) + '#' + str(label.numpy()) + '\n')
             batch_counter += 1
         file.close()
+
+
+def load_saved_pred(filepath):
+    prediction, true_label, batches = [], [], []
+    counter = 0
+    with open(filepath, 'r') as myfile:
+        data = myfile.read()
+        data = data.split('Epoch')
+        for epoch in data:
+            if not len(epoch.split('batch')) > 1:
+                continue
+            batches = epoch.split('batch')
+            for pred in batches:
+                if not len(epoch.split('batch')) > 1:
+                    continue
+                single_pred = pred.split('\n')
+                for sp in single_pred:
+                    if len(sp.split('#')) > 1:
+                        value = sp.split('[[')[1].split(']')[0]
+                        label = sp.split('#[')[1].split(']')[0]
+                        value = re.sub("\s+", ",", value.strip())
+                        value = [float(x) for x in value.split(',')]
+                        value = softmax_to_binary_value(value)
+                        label = re.sub("\s+", ",", label.strip())
+                        label = [int(x) for x in label.split(',')]
+                        prediction.append(value)
+                        true_label.append(label)
+
+            yield prediction, true_label
+
+
+def confusion_matrix(prediction, true_label,confusion_matrix):
+
+    if true_label == [1, 0, 0, 0, 0]:
+        confusion_matrix[0] = v_add(confusion_matrix[0],prediction)
+    elif true_label == [0, 1, 0, 0, 0]:
+        confusion_matrix[1] = v_add(confusion_matrix[1],prediction)
+    elif true_label == [0, 0, 1, 0, 0]:
+        confusion_matrix[2] = v_add(confusion_matrix[2],prediction)
+    elif true_label == [0, 0, 0, 1, 0]:
+        confusion_matrix[3] = v_add(confusion_matrix[3],prediction)
+    elif true_label == [0, 0, 0, 0, 1]:
+        confusion_matrix[4] = v_add(confusion_matrix[4],prediction)
+    else:
+        print('Error Matrix Addition')
+
+    return confusion_matrix
+
+
+def v_add(vec1, vec2):
+    return [vec1[0]+vec2[0],vec1[1]+vec2[1],vec1[2]+vec2[2],vec1[3]+vec2[3],vec1[4]+vec2[4]]
+
+
+def softmax_to_binary_value(prediction):
+    index = 0
+    value = prediction[0]
+
+    for numbers in range(1, len(prediction)):
+        if value < prediction[numbers]:
+            value = prediction[numbers]
+            index = numbers
+
+    for numbers in range(0, len(prediction)):
+        if numbers == index:
+            prediction[numbers] = 1
+        else:
+            prediction[numbers] = 0
+
+    return prediction
+"""
+prediction_iter = load_saved_pred('C:/Users/120392/Desktop/Training/history/confusion_matrix.txt')
+
+for pred, label in prediction_iter:
+    confusion_matrix2 = [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]]
+    for pre, lbl in zip(pred,label):
+        confusion_matrix(pre, lbl, confusion_matrix2)
+    value_in_matrix = 0
+    for row in confusion_matrix2:
+        print(row)
+        for x in row:
+            value_in_matrix += x
+
+    print(value_in_matrix)
+    """
